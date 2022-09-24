01c287476924f6424a211bb6aa342c0c03ac439e
Code Lines <codelines@Home.local>
2022-09-23 23:35:02 -0400

    complete landing page

--- forms.py
+++ forms.py
@@ -1,3 +1,7 @@
+from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DateField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, Flask
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import current_user, logout_user, login_required, UserMixin, login_user, login_manager
 from datetime import datetime
+
+
+
+
+# App initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Assddk8hsMt3hhy'
+
+@app.route('/')
+def home():
+    today = datetime.today().strftime('%d')
+    return render_template('index.html', today=today)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
